88d41a0f02527342359a840d211ad0f0df3e1a80
Bahodir Rajabov <rbcoder12345@gmail.com>
2025-12-06 01:29:53 -0800

    remove terminal

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,12 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from '@tailwindcss/vite'
-import Terminal from 'vite-plugin-terminal'
 
 export default defineConfig({
   plugins: [
     react(), 
     cloudflare(), 
     tailwindcss(),
-    Terminal({
-      console: 'terminal',
-      output: ['console', 'terminal'] 
-    })
   ],
   css: {
     devSourcemap: true,
